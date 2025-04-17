cc8e26f64ec3797e27d2f22509ccbc9b3024d03b
Carlos Alonso-Martinez <carlos.martinez@mataro.epiaedu.cat>
2025-04-17 09:55:58 +0200

    Fix: Array access in creaGrupsUsuaris.sh

--- creaGrupsUsuaris.sh
+++ creaGrupsUsuaris.sh
@@ -10,29 +10,28 @@ if [ "$EUID" -ne 0 ]; then
 fi
 
 # Llista de grups
-grups=("professors" "alumnes")
+grups=(professors alumnes)
 # Llista de professors
-professors=("professor1" "professor2" "professor3")
+professors=(professor1 professor2 professor3)
 # Llista d'alumnes
-students=("ofimatica" "disseny" "programacio")
+students=(ofimatica disseny programacio)
 # Llista per posar les contrasenyes inicials
-passwords=("pass1" "pass2" "pass3" "ofipass" "dispass" "progpass")
 
 # Crear grups
-for group in grups
+for grup in ${grups[*]}
 do
-    if ! getent group "$group" > /dev/null; then
-        groupadd "$group"
-        echo "Group $group created."
+    if ! getent group "$grup" > /dev/null; then
+        groupadd $group
+        echo "Grup $grup created."
     else
-        echo "Group $group already exists."
+        echo "Grup $grup already exists."
     fi
 done
 
 # Crear professors
-for user in professors
+for user in ${professors[*]}
 do
-    password="${passwords[$i]}"
+    password="123456" # Contrasenya inicial per a tots els professors
     if ! id "$user" > /dev/null 2>&1; then
         useradd -m -G professors "$user"
         echo "$user:$password" | chpasswd -c SHA512
@@ -43,12 +42,10 @@ do
 done
 
 # Crear alumnes
-for user in students
+for user in ${students[*]}
 do
-    password="${passwords[$i + 3]}" # Desplaçament per passwords alumnes
     if ! id "$user" > /dev/null 2>&1; then
         useradd -m -G alumnes "$user"
-        echo "$user:$password" | chpasswd -c SHA512
         echo "Usuari $user creat i afegit al grup alumnes."
     else
         echo "L'usuari $user ja existeix."
